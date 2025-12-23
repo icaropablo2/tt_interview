46a75b0230420264fdb382b848f4622aa500b9c3
Icaro Trindade <icaropablo2@gmail.com>
2025-12-23 13:57:35 -0300

    last commit

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from venmo_exceptions import UsernameException
+from venmo_exceptions import UsernameException,PaymentException
 from user import User
 from main import MiniVenmo
 
@@ -12,7 +12,7 @@ def test_user_payment():
 def test_user_payment__():
     minivenmo = MiniVenmo()
     bobby = minivenmo.create_user("Bobby", 0, "4111111111111111")
-    carol = minivenmo.create_user("Carol", 10, "4242424242424242")
+    carol = minivenmo.create_user("Bobby", 10, "4242424242424242")
 
-    
-    payment = bobby.pay(carol, 5.00, "Coffee")
+    with pytest.raises(PaymentException) as error:
+        payment = bobby.pay(carol, 5.00, "Coffee")
