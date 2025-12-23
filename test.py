8f5cc3c21be3f6e6e4a36189737a2c03884ccb42
Icaro Trindade <icaropablo2@gmail.com>
2025-12-23 13:55:04 -0300

    check if balance condition

--- test.py
+++ test.py
@@ -8,5 +8,11 @@ def test_user_payment():
     # this method don't allow me to test
     minivenmo = MiniVenmo()
     minivenmo.run()
-    
 
+def test_user_payment__():
+    minivenmo = MiniVenmo()
+    bobby = minivenmo.create_user("Bobby", 0, "4111111111111111")
+    carol = minivenmo.create_user("Carol", 10, "4242424242424242")
+
+    
+    payment = bobby.pay(carol, 5.00, "Coffee")
