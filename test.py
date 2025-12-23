4e563c5c9329ef6a2e1716ff774d4e088c8f02a4
Icaro Trindade <icaropablo2@gmail.com>
2025-12-23 13:50:18 -0300

    starting testing

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import pytest
 
 from venmo_exceptions import UsernameException
+from user import User
+from main import MiniVenmo
 
 def test_user_payment():
-    assert True is False
+    # this method don't allow me to test
+    minivenmo = MiniVenmo()
+    minivenmo.run()
+    
 
